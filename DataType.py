c9642f0e5640e9ab828073824a50307d2f52f7b5
suwani <suwanikenulya@gmail.com>
2026-04-07 22:46:10 +0530

    data types

---
+++ DataType.py
@@ -0,0 +1,29 @@
+#data types
+# 1. strings
+#print(type("Hello World"))
+#print ("hello world"[0]) # we can type a letter by it's index
+
+#2. integer
+#print (type(5))
+
+#3. float
+#print (type(5.67))
+
+#4. boolian
+#true or false
+
+#-----note is done---
+
+fnum= input("enter a number")
+snum= input ("enetr a second number")
+
+if fnum > snum:
+    print ("the first number is larger.")
+elif fnum < snum:
+    print ("the second number is smaller.")
+else:
+    print("the numbers are the same.")
+
+
+    #loops
+
