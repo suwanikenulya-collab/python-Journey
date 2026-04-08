161d0cd9aeae311c93aa1e545881ff20bfca0237
suwani <suwanikenulya@gmail.com>
2026-04-08 08:42:30 +0530

    fuctions in python practice.

---
+++ functions.py
@@ -0,0 +1,20 @@
+from time import sleep
+import time
+choice=input("enter your choice")
+choice=int(choice)
+
+def function(choice):
+    for num in range(1, choice):
+        if num % 3 == 0 and num % 5 == 0:
+            print("fizzbuzz")
+        elif num % 3 == 0:
+            print("fizz")
+        elif num % 5 == 0:
+            print("buzz")
+        else:
+            print(num)
+
+
+print("the program is about to start.")
+time.sleep(5)
+function(choice)
