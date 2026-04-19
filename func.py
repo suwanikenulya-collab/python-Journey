ef78a14f9ed9e5622bbca7e349fbf50f03a1d4c6
suwani <suwanikenulya@gmail.com>
2026-04-19 13:54:30 +0530

    define function note - practice day

---
+++ func.py
@@ -0,0 +1,18 @@
+#functions note-02
+ip=input("Enter your ip address:")
+
+def attack(i):
+    print(i)
+
+attack(ip)
+
+def call(k):
+    print(k)
+
+call("10.10.12.11")
+
+
+def affect(i,d):
+    print(f"you can use {i} and {d}")
+
+affect(56, "hihiii")
