3963f9c45c510341700ec840e01c799a27edf42b
suwani <suwanikenulya@gmail.com>
2026-05-03 09:04:42 +0530

    Experiment with Python module imports and main function structure

---
+++ app.py
@@ -0,0 +1,12 @@
+import app
+
+app.main()
+
+def main():
+    print(f"file2 name is {__name__}")
+
+#def main():
+    #print(f"Hello World dear file {__name__}")
+
+#if __name__ == "__main__":
+    #main()
