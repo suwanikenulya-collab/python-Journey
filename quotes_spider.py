b12e84d88bafc13d3223a3b6e35b05fcfa373f60
Suwani Kenulya <suwanikenulya@gmail.com>
2026-06-05 22:16:50 +0530

    explained each line.

--- quotes_spider.py
+++ quotes_spider.py
@@ -1,12 +1,27 @@
+# Import the Scrapy library
 import scrapy
 
+
+# Create a spider (the web-crawling robot)
 class QuotesSpider(scrapy.Spider):
+
+    # Give the spider a name
+    # This is used when running the spider
     name = "quotes"
 
+    # List of websites where the spider should start
     start_urls = [
         "https://quotes.toscrape.com/"
     ]
 
+    # This method runs automatically when a page is downloaded
     def parse(self, response):
+
+        # Find all quote text on the page
+        # CSS selector:
+        # span.text = find <span class="text">
+        # ::text = get only the text inside it
         for quote in response.css("span.text::text"):
+
+            # Extract the actual text and print it
             print(quote.get())
