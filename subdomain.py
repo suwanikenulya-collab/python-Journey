a005aa5724b7861949235bb75151661264b4c1f3
Suwani Kenulya <suwanikenulya@gmail.com>
2026-06-09 16:32:29 +0530

    done the psuedocode

--- subdomain.py
+++ subdomain.py
@@ -1,3 +1,32 @@
-domain = input("Enter a domain: ")
+import socket
 
-print("You entered:", domain)
+# Get domain from user
+domain = input("Enter a domain (e.g. google.com): ")
+
+# Common subdomains to check
+subdomains = [
+    "www",
+    "mail",
+    "admin",
+    "api",
+    "blog",
+    "dev",
+    "test",
+    "ftp",
+    "shop",
+    "portal"
+]
+
+print("\nSearching for subdomains...\n")
+
+for subdomain in subdomains:
+    full_domain = f"{subdomain}.{domain}"
+
+    try:
+        ip = socket.gethostbyname(full_domain)
+        print(f"[FOUND] {full_domain} -> {ip}")
+
+    except socket.gaierror:
+        pass
+
+print("\nScan complete!"
